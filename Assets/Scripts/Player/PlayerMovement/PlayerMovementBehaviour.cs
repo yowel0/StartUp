356d1fa98e33a174f44eaad38ed8f725fc755bdf
SaraHesselink05 <548317@student.saxion.nl>
2025-01-21 15:53:48 +0100

    footsteps yay

--- Assets/Scripts/Player/PlayerMovement/PlayerMovementBehaviour.cs
+++ Assets/Scripts/Player/PlayerMovement/PlayerMovementBehaviour.cs
@@ -1,4 +1,5 @@
 
+using FMODUnity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,7 +8,9 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
+using FMODUnity;
 using FMOD.Studio;
+
 /// <summary>
 /// This class still doesn't follow single responsibility principle because
 /// it also handles animations, we'll fix this in bootcamp 2 using Observer pattern.
@@ -22,9 +25,16 @@ public abstract class MoveBehaviour : MonoBehaviour
     [SerializeField] LayerMask whatIsGround;
 
     [SerializeField] float maxSlopeAngle;
+
+    [SerializeField]
+    private EventReference footstepEvent;
+    
+    private float footstepDelay = 0.5f; // Delay between footstep sounds
+    private float lastFootstepTime = 0f;
+
+
     private RaycastHit slopeHit;
-    //for footsteps
-    private EventInstance playerFootsteps;
+    
 
     public bool grounded;
     [SerializeField] bool onSlope;
@@ -53,8 +63,9 @@ public abstract class MoveBehaviour : MonoBehaviour
         playerOr = transform;
         mAnimator = GetComponent<Animator>();
         PlayIdleAnimation();
-        //for footsteps
-        playerFootsteps = AudioManager.instance.CreateEventInstance(FMODEvents.instance.doFootsteps);
+
+        
+
     }
 
     public void Update()
@@ -92,21 +103,15 @@ public abstract class MoveBehaviour : MonoBehaviour
         if (directionMoving.magnitude == 0)
         {
             rb.isKinematic = true;
-
-            //footsteps
-            playerFootsteps.stop(STOP_MODE.ALLOWFADEOUT);
+           
+           
         }
         else
         {
             rb.isKinematic = false;
+            PlayFootstepSound();
 
-            //footsteps
-            PLAYBACK_STATE playbackState;
-            playerFootsteps.getPlaybackState(out playbackState);
-            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-            {
-                playerFootsteps.start();
-            }
+            
 
 
         }
@@ -155,6 +160,22 @@ public abstract class MoveBehaviour : MonoBehaviour
             state = States.walking;
             speedMult = 1;
         }
+    }
+
+
+    private void PlayFootstepSound()
+    {
+        if (Time.time >= lastFootstepTime + footstepDelay && directionMoving.magnitude > 0)
+        {
+            RuntimeManager.PlayOneShot(footstepEvent, transform.position);
+            lastFootstepTime = Time.time;
+        }
+
+    }
+  
+    private void OnDestroy()
+    {
+        // Ensure the event instance is released when the object is destroyed
     }
 
     #region "animation related fields"
