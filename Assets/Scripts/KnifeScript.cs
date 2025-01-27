a87fa781013fbf875114296f53a39c7a592a630f
SaraHesselink05 <548317@student.saxion.nl>
2025-01-27 12:25:13 +0100

    added some sounds again

--- Assets/Scripts/KnifeScript.cs
+++ Assets/Scripts/KnifeScript.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class KnifeScript : MonoBehaviour
 {
     public bool grabbed = false;
+
+
+    [SerializeField]
+    private EventReference killCop;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,7 @@ public class KnifeScript : MonoBehaviour
         if (other.gameObject.CompareTag("Police") && grabbed)
         {
             print("b");
+            RuntimeManager.PlayOneShot(killCop, transform.position);
             other.gameObject.GetComponent<PoliceBehaviour>().enabled = false;
             other.transform.rotation = Quaternion.Euler(-90, 0, 0);
             other.transform.GetComponent<Rigidbody>().isKinematic = false;
