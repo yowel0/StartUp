366be06b1410205db8113454b53d6df4b17fc9f0
Thijmen <thijmen.mennegat@gmail.com>
2025-01-28 14:00:16 +0100

    evidence changes

--- Assets/EvidenceSpawner.cs
+++ Assets/EvidenceSpawner.cs
@@ -1,33 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class EvidenceSpawner : MonoBehaviour
 {
     public List<Evidence> m_EvidenceList;
-
-    public GameObject[] Windows;
+    public GameObject[] windows;
+    public GameObject brokenWindow;
     void Start()
     {
         foreach (Evidence evidence in m_EvidenceList)
         {
             evidence.SpawnRandom();
         }
+        SpawnWindow();
     }
+    public void SpawnWindow()
+    {
+        int spawnIndex = UnityEngine.Random.Range(0, windows.Length);
+        brokenWindow = Instantiate(brokenWindow, windows[spawnIndex].transform);
+        windows[spawnIndex].gameObject.SetActive(false);
+    }
+
 
     [Serializable]
     public class Evidence
     {
         public string name = "evidence";
         public GameObject evidenceObj;
+
         public Transform[] spawnPositions;
-        
+
         public void SpawnRandom()
         {
             int spawnIndex = UnityEngine.Random.Range(0, spawnPositions.Length);
             evidenceObj = Instantiate(evidenceObj, spawnPositions[spawnIndex]);
         }
-
+        
     }
+   
 }
