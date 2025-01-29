b651a79da38cdf80ae068d688b441b664880bf3a
Thijmen <thijmen.mennegat@gmail.com>
2025-01-29 16:28:06 +0100

    i dont even know atp

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] LayerMask doorLayer;
+    Camera cam;
+    Transform hinge;
+    float maxRot = 90;
+    [SerializeField] float rot = 0;
+    bool gotDoor = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 10, doorLayer))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hinge = hit.transform.parent.transform;
+                rot = hinge.transform.rotation.y * 120;
+                print(rot);
+                gotDoor = true;
+            }
+        }
+        if (gotDoor) {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * 400;
+            rot -= mouseX;
+            rot = Mathf.Clamp(rot, 0, 90f);
+            hinge.rotation = Quaternion.Euler(0, hinge.rotation.y + rot, 0);
+        }
+        if (Input.GetMouseButtonUp(0) && hinge)
+        {
+            hinge = null;
+            gotDoor = false;
+            rot = 0;
+        }
+    }
+}
