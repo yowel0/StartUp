ee4e20db9e06f350e4ac820b452553c774ba4c77
R-Bleach <loljk366@gmail.com>
2025-01-30 14:28:23 +0100

    Murderer faster and easier to stab cop

--- Assets/Scripts/KnifeScript.cs
+++ Assets/Scripts/KnifeScript.cs
@@ -6,15 +6,34 @@ public class KnifeScript : MonoBehaviour
 {
     public bool grabbed;
     ThiefBehaviour tb;
+    BoxCollider box;
+    float timer;
+    bool rah;
     // Start is called before the first frame update
     void Start()
     {
         tb = GetComponentInParent<ThiefBehaviour>(true);
+        box = GetComponentInParent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            box.enabled = true;
+            rah = true;
+        }
+        if (rah)
+        {
+            timer += Time.deltaTime;
+            if(timer >= 1)
+            {
+                box.enabled = false;
+                rah = false;
+                timer = 0;
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
